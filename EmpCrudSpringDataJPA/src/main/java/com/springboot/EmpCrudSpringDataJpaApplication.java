9c0d4075abf58ecde04acedf05cbde358bf9eb8d
DIVYA <DIVYA@LAPTOP-JTHODNGL>
2020-04-04 22:32:47 +0530

    commit

---
+++ EmpCrudSpringDataJPA/src/main/java/com/springboot/EmpCrudSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpCrudSpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpCrudSpringDataJpaApplication.class, args);
+	}
+
+}
